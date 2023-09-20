0c75f5327827a730885b03cea4eaa366d5de266a
khanghungvo1211 <92728112+khanghungvo1211@users.noreply.github.com>
2023-09-20 19:49:13 +0700

    first commit

---
+++ controller/userController.js
@@ -0,0 +1,32 @@
+const e = require('express');
+const User = require('../models/userModel');
+const asynchHandler = require('express-async-handler');
+
+const createUser = asynchHandler(async (req, res) => {
+    const email = req.body.email;
+    const findUser = await User.findOne({ email: email });
+
+    if (!findUser) {
+        // Create a new user
+        const newUser = await User.create(req.body);
+        res.json(newUser);
+    } else {
+        throw new Error('User Already Exists');
+    }
+});
+
+const loginUserController = asynchHandler(async (req, res) => {
+    const { email, password } = req.body;
+    //check if user exists or not
+    const findUser = await User.findOne({email});
+    if (findUser && (await findUser.isPasswordMatched(password))){   
+        res.json(findUser);
+
+    } else{
+        throw new Error("Invalid Credentials");
+    }
+
+
+});
+
+module.exports = { createUser, loginUserController };
---
+++ routes/authRoute.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const { createUser, loginUserController } = require("../controller/userController");
+const router = express.Router();
+router.post("/register", createUser);
+router.post("/login", loginUserController);
+module.exports = router;
